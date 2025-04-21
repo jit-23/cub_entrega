55357505c1f64be937a7673d317ae59f4b51246b
Fernando De Jesus Lucart <fde-jesu@c1r10s6.42porto.com>
2025-04-21 19:45:04 +0100

    lol

--- srcs/calc_rays.c
+++ srcs/calc_rays.c
@@ -6,7 +6,7 @@
 /*   By: fde-jesu <fde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 00:12:08 by fde-jesu          #+#    #+#             */
-/*   Updated: 2025/04/20 01:06:51 by fde-jesu         ###   ########.fr       */
+/*   Updated: 2025/04/21 19:44:38 by fde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,11 @@ void draw_line(t_cub *cub, t_rays *rays, int i)
 
 	column = -1;
 	// ceiling
-	while( ++column < rays->drawStart)
+	while(++column < rays->drawStart)
 		my_mlx_pixel_put(&cub->imgs[1], i, column, 0xFFFF);
 	// floor
 	column = rays->drawEnd;
-	
-	while( ++column < HEIGH)
+	while(++column < HEIGH)
 		my_mlx_pixel_put(&cub->imgs[1], i, column, 0x0F0F);
 	// walls
 	while (rays->y < rays->drawEnd) 
@@ -67,65 +66,66 @@ void ft_calc_ray_side(t_cub *cub, t_rays **ray)
 	} 
     else
     {
-	    rays->stepY = 1; // cima
+		rays->stepY = 1; // cima
 	    rays->sideDistY = (rays->mapY + 1.0 - cub->py) * rays->deltaDistY;
-    }
-}
-
-bool colision(float px, float py, t_cub *cub)
-{
-	int x;
-	int y;
-
-    x = (px / BLOCK);
-    y = (py / BLOCK);
-	if (cub->map[y][x] == '1')
-		return true;
-	return false;
-}
-
-void ft_calc_dda(t_cub *cub, t_rays **ray)
-{
-	t_rays  *rays;
-
-    rays = (*ray);
-	rays->hit = 0;
-	while (rays->hit == 0)// Move in the direction of the shortest step 
-    {
-	    if (rays->sideDistX < rays->sideDistY)
-        {
-	    	rays->sideDistX += rays->deltaDistX;
-	    	rays->mapX += rays->stepX;
-	    	rays->side = 0;
-	    }
-        else
-        {
-	    	rays->sideDistY += rays->deltaDistY;
-	    	rays->mapY += rays->stepY;
-	    	rays->side = 1;
-	    }
-	    if (colision(rays->mapX, rays->mapY, cub))// Check for a collision (wall hit) 
-            rays->hit = 1;
 	}
 }
 
-
-void init_rays( t_cub *cub, t_rays *rays, int i)
+bool	colision(float px, float py, t_cub *cub)
 {
-	rays->mapX = (int)cub->px;  
-	rays->mapY = (int)cub->py; 
-	rays->fov = PI/3;
+	int	x;
+	int	y;
+
+	x = (px / BLOCK);
+	y = (py / BLOCK);
+	if (cub->map[y][x] == '1')
+		return true;
+	return (false);
+}
+
+void	ft_calc_dda(t_cub *cub, t_rays **ray)
+{
+	t_rays	*rays;
+
+	rays = (*ray);
+	rays->hit = 0;
+	while (rays->hit == 0) // Move in the direction of the shortest step 
+	{
+		if (rays->sideDistX < rays->sideDistY)
+		{
+			rays->sideDistX += rays->deltaDistX;
+			rays->mapX += rays->stepX;
+			rays->side = 0;
+		}
+		else
+		{
+			rays->sideDistY += rays->deltaDistY;
+			rays->mapY += rays->stepY;
+			rays->side = 1;
+		}
+		if (colision(rays->mapX, rays->mapY, cub)) // Check for a collision (wall hit)
+			rays->hit = 1;
+	}
+}
+
+void	init_rays( t_cub *cub, t_rays *rays, int i)
+{
+	rays->mapX = (int)cub->px;
+	rays->mapY = (int)cub->py;
+	rays->fov = PI / 3;
 	rays->planeLength = tan(rays->fov / 2);
 	rays->dirX = cos(cub->angle);
 	rays->dirY = sin(cub->angle);
 	rays->planeX = -rays->dirY * rays->planeLength;
-	rays->planeY =  rays->dirX * rays->planeLength;
+	rays->planeY = rays->dirX * rays->planeLength;
 	rays->cameraX = 2 * i / (double)WIDTH - 1;
 	rays->rayDirX = rays->dirX + rays->planeX * rays->cameraX;
 	rays->rayDirY = rays->dirY + rays->planeY * rays->cameraX;
-	rays->deltaDistX = sqrt(1 + (rays->rayDirY * rays->rayDirY) / (rays->rayDirX * rays->rayDirX));
-	rays->deltaDistY = sqrt(1 + (rays->rayDirX * rays->rayDirX) / (rays->rayDirY * rays->rayDirY));
+	rays->deltaDistX = sqrt(1 + (rays->rayDirY * rays->rayDirY) \
+		/ (rays->rayDirX * rays->rayDirX));
+	rays->deltaDistY = sqrt(1 + (rays->rayDirX * rays->rayDirX) \
+		/ (rays->rayDirY * rays->rayDirY));
 	ft_calc_ray_side(cub, &rays);
 	ft_calc_dda(cub, &rays);
-	ft_calc_dist_wall(cub,&rays, i);
+	ft_calc_dist_wall(cub, &rays, i);
 }
